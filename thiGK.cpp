15476bd77ca14640b0003a1a4edc76e6568fa361
Dang <dang.nguyehai2k2@gmail.com>
2023-11-13 16:50:56 +0700

    ktsht

--- thiGK.cpp
+++ thiGK.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
+
+int Check_SHT(int a)
+{
+    int b = 0;
+    for (int i = 1; i < N; i++)
+        if (a % i == 0)
+            b += i;  // S = S + i
+
+    if (b == a)
+        return true; // Là số hoàn thiện
+    return false;     // Không là số hoàn thiện
+}
 int main(){
     int a;
     cin>>a;
     cout << int(a);
+    ktSoHT(a);
     return 0;
-    
+
 }
