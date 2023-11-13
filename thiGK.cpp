31bf8dff6b2eed036c22d92dec090e5ceb5f3af1
Dang <dang.nguyehai2k2@gmail.com>
2023-11-13 16:51:13 +0700

    ktscp

--- thiGK.cpp
+++ thiGK.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 using namespace std;
+int ktsoCP(int a){
+	int i = 0;
+	while(i*i <= a){
+		if(i*i == a){
+			cout << a << " la so chinh phuong!\n";
+			return 0;
+		}
+		++i;
+	}
+	cout << n << " khong phai so chinh phuong!\n";
+}
 int main(){
     int a;
     cin>>a;
